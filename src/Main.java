63ac9b0f31d52f850303d504dbc036f88b0dde60
Ivan Andreev <fc97@mail.ru>
2023-03-12 20:51:35 +0400

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
     static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+
+        WordsChecker wd = new WordsChecker(text);
+        System.out.println(wd.hasWord("in"));
+
         Map<Character, Integer> map = new HashMap<>();
         char[] e = text.toLowerCase().toCharArray();
 
